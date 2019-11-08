c81417fc9a46ceb0e36d9589c98229466ce75ba5
lbalotta <lbalotta@transferoil.com>
2019-11-08 14:49:20 +0100

    AnimController + BaseCreature

---
+++ MagicOnionTest/Assets/Scripts/Game/BaseCreature.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCreature : MonoBehaviour
+{
+    Animator m_Animator;
+
+    private bool m_Jump;
+
+    private bool m_Walking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.U))
+            m_Animator.SetBool("IsWalking", !m_Walking);
+    }
+}
