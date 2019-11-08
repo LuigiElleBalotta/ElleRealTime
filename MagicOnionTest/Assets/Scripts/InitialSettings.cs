a29aaa225eaf1c476378186d1f83f940b6c559c8
lbalotta <lbalotta@transferoil.com>
2019-11-08 10:56:13 +0100

    Imported Generators

---
+++ MagicOnionTest/Assets/Scripts/InitialSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MessagePack.Resolvers;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class InitialSettings
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void RegisterResolvers()
+        {
+            CompositeResolver.RegisterAndSetAsDefault
+            (
+                MagicOnion.Resolvers.MagicOnionResolver.Instance,
+                //MessagePack.Resolvers.DynamicGenericResolver.Instance,
+                BuiltinResolver.Instance,
+                PrimitiveObjectResolver.Instance
+            );
+        }
+    }
+}
+
